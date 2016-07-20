c7f0c042a45df83fd978c3e33d74b2f15f481897
tallisprog <tallismgf@gmail.com>
2016-07-20 17:56:24 -0300

    Update conexao.java

--- conexao.java
+++ conexao.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logica;
 
-/**
- *
- * @author Eduardo de Andrade
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+
+
 public class conexao {
+    public String bd = "b.reserva";
+    public String url = "jdbc:mysql://127.0.0.1/" + bd;
+    public String user = "root";
+    public String pass = "";
+
+
+public conexao(){
     
+}
+
+public Connection conectar(){
+    Connection link = null;
+    try {
+        Class.forName("org.gjt.mm.mysql.Driver");
+        link = DriverManager.getConnection(this.url, this.user, this.user);
+    }
+    catch (ClassNotFoundException | SQLException e){
+        JOptionPane.showConfirmDialog(null, e);
+    }
+    return link;
+}
 }
